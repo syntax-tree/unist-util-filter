cfb036cc0defef3211175f8bf2f722ebc6a49461
Christian Murphy <Christian.Murphy.42@gmail.com>
2019-10-27 21:33:10 -0700

    test: remove duplicated type test

--- types/unist-util-filter-tests.ts
+++ types/unist-util-filter-tests.ts
@@ -7,6 +7,5 @@ filter({type: 'root'}, 'root') // $ExpectType Node | null
 filter({type: 'root'}, {}, 'root') // $ExpectType Node | null
 filter({type: 'root'}, {notAnOption: true}, 'root') // $ExpectError
 filter({type: 'root'}, {cascade: false}, 'root') // $ExpectType Node | null
-filter({type: 'root'}, {cascade: false}, 'root') // $ExpectType Node | null
 filter<Heading>({type: 'root'}, 'heading') // $ExpectType Heading | null
 filter<Heading>({type: 'root'}, 'notAHeading') // $ExpectError
